cf2c1c1951bf51a1e122b8101408969ea6b8acd2
Maryia Rusinovich <maryrusinovich@gmail.com>
2023-01-10 20:35:34 +0200

    change gitignore

--- test.ts
+++ test.ts
@@ -1,1 +1,16 @@
-console.log('Check test');
+function fizzbuzz(n: number): string | number {
+    if (n % 3 === 0 && n % 5 === 0){
+        return 'FizzBuzz';
+    }
+    if (n % 5 === 0){
+        return 'Buzz';
+    }
+    if (n % 3 === 0){
+        return 'Fizz';
+    }
+    return n;
+}
+
+for (let i: number = 1; i <= 100; i++){
+    console.log(fizzbuzz(i));
+}
